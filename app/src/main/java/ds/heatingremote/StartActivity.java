a3acdceb71be30aa75aa2f07f95f66f5c2f25496
Damian-R-Smith <damiansmith@live.ie>
2016-01-06 03:28:19 +0000

    cleaning up code

--- app/src/main/java/ds/heatingremote/StartActivity.java
+++ app/src/main/java/ds/heatingremote/StartActivity.java
@@ -1,10 +1,8 @@
 package ds.heatingremote;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import java.io.IOException;
@@ -18,6 +16,7 @@ import io.particle.android.sdk.utils.Toaster;
 /**
  * Created by Damian Smith on 03/01/2016.
  * Twitter @Javawocky
+ * Email javawocky.ds@gmail.com
  */
 
 public class StartActivity extends Activity {
@@ -32,28 +31,9 @@ public class StartActivity extends Activity {
         findViewById(R.id.connect_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                attemptDeviceConnect();
+                login();
             }
         });
-    }
-
-    public void attemptDeviceConnect(){
-        final ProgressDialog mProgressDialog = ProgressDialog.show(
-                StartActivity.this, "One Sec Mary....", "Connecting to controller device...", true);
-        mProgressDialog.setCancelable(true);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try{
-                    login();
-                    Thread.sleep(30000);
-                }catch (Exception e){
-                    Log.e("ERR", "Thread error");
-                }
-            }
-
-        }).start();
-        mProgressDialog.dismiss();
     }
 
     public void login(){
@@ -63,10 +43,9 @@ public class StartActivity extends Activity {
 
             @Override
             public Object callApi(ParticleCloud sparkCloud) throws ParticleCloudException, IOException {
-                sparkCloud.logIn(getString(R.string.un), getString(R.string.pw));
+                sparkCloud.logIn("YOUR_LOGIN_EMAIL", "YOUR_PASSSWORD");
                 sparkCloud.getDevices();
-                mDevice = sparkCloud.getDevice("37001a000c47343432313031");
-                Object obj;
+                mDevice = sparkCloud.getDevice("YOUR_DEVICE_ID");
                 return -1;
             }
 
@@ -78,18 +57,17 @@ public class StartActivity extends Activity {
                 if(mDevice.isConnected()){
                     Intent intent = new Intent(StartActivity.this, RemoteControlActivity.class);
                     intent.putExtra(ARG_VALUE, value.toString());
-                    intent.putExtra(ARG_DEVICEID, "37001a000c47343432313031");
+                    intent.putExtra(ARG_DEVICEID, "YOUR_DEVICE_ID");
                     startActivity(intent);
                 }else{
-                    Toaster.l(StartActivity.this, "Unable to connect device. Please try again.");
+                    Toaster.l(StartActivity.this, "Unable to connect device");
                 }
             }
 
             @Override
             public void onFailure(ParticleCloudException e) {
-                Toaster.l(StartActivity.this,"Something has gone horribly wrong!!! ");
+                Toaster.l(StartActivity.this, "Something has gone horribly wrong!!! ");
                 e.printStackTrace();
-                Log.d("info", e.getBestMessage());
             }
 
         });
